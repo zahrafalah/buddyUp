107071208f6947ad1a7aea425cc1fbc7ca0c91e9
Briana Oursler <briana.oursler@gmail.com>
2018-07-02 13:15:03 -0700

    Replace boilerplate with specific render-target, import React and provide example of semantic-ui-react usage in top-level App.js

---
+++ buddy/imports/ui/App.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+import { Button } from 'semantic-ui-react'
+
+export default class App extends Component {
+	
+	render() {
+	  return (
+		<Button> Click Here </Button>
+	  );
+	}
+  }
