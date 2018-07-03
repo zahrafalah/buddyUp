c4a858cbbd0e42bec5f97316013b4fcec3c69993
zahra falah <sepidehfallah2@gmail.com>
2018-07-03 12:17:39 -0700

    getting homepage to show in the browser using app&routs.js

--- buddy/imports/ui/App.js
+++ buddy/imports/ui/App.js
@@ -1,13 +1,17 @@
 
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
+import HomePage from './pages/HomePage'
 
 export default class App extends Component {
 	
 	render() {
 	  return (
-		<Button> Click Here </Button>
+
+		<HomePage  />
+
 	  );
 	}
-  }
+	}
+
