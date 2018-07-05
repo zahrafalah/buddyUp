d16284a4241afa1d1cfec2e0769a2ff85eef9179
Aceituna <miriam_armada@yahoo.com>
2018-07-05 08:58:59 -0700

    imaes

--- buddy/client/main.js
+++ buddy/client/main.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-//import client-side routing and configs
-import '/imports/startup/client';
-=======
 
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
->>>>>>> dda7e0746200cb3a88b403d37c1370b1f2384c0d
 import './main.html';
 
 import App from '../imports/ui/App.js';
