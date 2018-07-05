b0f85a770c0760f0663b121fa7ddf866355832af
Briana Oursler <briana.oursler@gmail.com>
2018-07-05 11:33:05 -0700

    Basic test to fix bug where tests don't run

---
+++ buddy/imports/api/profiles/profiles.tests.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+import { Meteor } from 'meteor/meteor';
+
+describe('Tasks', () => {
+describe('methods', () => {
+it('can delete owned task', () => {
+console.log("Testing to see if this is running");
+	});
+	});
+});
