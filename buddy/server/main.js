5f474c61598f6c0b391065de6fd39a5ff15538c6
Aceituna <miriam_armada@yahoo.com>
2018-07-05 11:39:02 -0700

    update

--- buddy/server/main.js
+++ buddy/server/main.js
@@ -1,10 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 //import server startup code
-<<<<<<< HEAD
 import '/imports/startup/server';
-=======
-import '../imports/startup/server';
->>>>>>> dda7e0746200cb3a88b403d37c1370b1f2384c0d
 Meteor.startup(() => {
   // code to run on server at startup
 });
