8d7a074e94a80037e237926cbacd56bd73d56a22
Aceituna <miriam_armada@yahoo.com>
2018-07-05 11:11:58 -0700

    update

--- buddy/imports/startup/server/index.js
+++ buddy/imports/startup/server/index.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
 import '../imports/startup/server/fixtures.js';
-=======
-import './fixtures.js';
->>>>>>> dda7e0746200cb3a88b403d37c1370b1f2384c0d
 
 // This file configures the Accounts package to define the UI of the reset password email.
 // import '../imports/startup/server/reset-password-email.js';
 
 // Set up some rate limiting and other important security settings.
-<<<<<<< HEAD
 import '../imports/startup/server/security.js';
 
 // This defines all the collections, publications and methods that the application provides
 // as an API to the client.
 import '../imports/api/api.js';
-=======
-import './security.js';
-
-// This defines all the collections, publications and methods that the application provides
-// as an API to the client.
-import '../../api';
->>>>>>> dda7e0746200cb3a88b403d37c1370b1f2384c0d
