2e2145b7560eddef7c82405798e42fad6c7a0ed0
Briana Oursler <briana.oursler@gmail.com>
2018-07-06 13:01:37 -0700

    Fix import statements, delete commented code and fix tests to actually validate schema

--- buddy/imports/api/profiles/profiles.tests.js
+++ buddy/imports/api/profiles/profiles.tests.js
@@ -1,63 +1,27 @@
 /* eslint-env mocha */
 
 import { Meteor } from 'meteor/meteor';
-
+import SimpleSchema from 'meteor/aldeed:collection2'
 import {Profiles} from './profiles.js';
+import { AssertionError } from 'assert';
 describe('Profiles', () => {
-	describe('insert', () => {
-		it('can insert a validated schema', () => {
-			Profiles.insert({firstName: "Ulysses", lastName: "Joyce"}, (error, result) => {
-				//The insert will fail, error will be set,
-				//and result will be undefined or false because "copies" is required.
-				//
-				if(error)
-					Profiles.simpleSchema().namedContext().validationErrors();
-				else {
-					console.log(result);
-					console.log(Profiles.find());
-				}
-				//The list of errors is available on `error.invalidKeys` or by calling Books.simpleSchema().namedContext().validationErrors()
-			  });
+	describe('invalid insert', () => {
+		it('denies invalid inserts into a validated schema', () => {
+			console.log("Setup to run build");
+			ret = Profiles.simpleSchema().namedContext().validate({title: "Ulysses", author: "James Joyce"}, {modifier: false});
+			if(ret == true)
+				throw new Error("Failed invalid schema accepted as valid");
 		});
 	});
+	describe('valid insert', () => {
+		it ('validates correct inputs into a schema', () => {
+			ret = Profiles.simpleSchema().namedContext().validate({firstName: "Joe", lastName: "Schmoe"}, {modifier: false});
+			if(ret === false)
+				throw new Error("Failed: valid schema denied as invalid");
+		})
+	})
 });
 
-// import faker from 'faker';
-// import chai from 'chai';
-// import { Factory } from 'meteor/dburles:factory';
 
-// Factory.define('profile', Profile, {
-//   firstName: 'John',
-//   lastName: 'Smith',
-//   bio: () => faker.lorem.sentence()
-  
-// });
-
-// describe('adding a profile', function () {
-// 	it('adds a user profile to the db', function () {
-// 	  // This code will be executed by the test driver when the app is started
-// 	  // in the correct mode
-// 	  beforeEach(function () {
-// 		resetDatabase();
-// 	  });
-// 	  try {
-// 	  const profile_run = Factory.create('profile');
-// 	  profile_run;
-// 	  }
-// 	  catch {
-// 		  throw error;
-// 	  }
-// 	})
-//   })
-// if(Meteor.isServer){
-// describe('my module', function () {
-//   it('does something that should be tested', function () {
-//     // This code will be executed by the test driver when the app is started
-//       // in the correct mode
-//       console.log("hello world");
-//   })
-// })
-// }
-/* eslint-env mocha */
 
 
