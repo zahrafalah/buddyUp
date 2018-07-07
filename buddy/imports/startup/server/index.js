019085c794195084561ef78505b253392b7c44e2
Aceituna <miriam_armada@yahoo.com>
2018-07-06 17:33:11 -0700

    update server side use Master

--- buddy/imports/startup/server/index.js
+++ buddy/imports/startup/server/index.js
@@ -1,11 +1,23 @@
+<<<<<<< HEAD
 import './fixtures.js';
+=======
+import '../imports/startup/server/fixtures.js';
+>>>>>>> 8d7a074... update
 
 // This file configures the Accounts package to define the UI of the reset password email.
 // import '../imports/startup/server/reset-password-email.js';
 
 // Set up some rate limiting and other important security settings.
+<<<<<<< HEAD
 import './security.js';
 
 // This defines all the collections, publications and methods that the application provides
 // as an API to the client.
 import '../../api';
+=======
+import '../imports/startup/server/security.js';
+
+// This defines all the collections, publications and methods that the application provides
+// as an API to the client.
+import '../imports/api/api.js';
+>>>>>>> 8d7a074... update
