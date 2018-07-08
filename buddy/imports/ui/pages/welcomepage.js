f72ee1f03d6256c51138dc0e023e135f59b3fd06
zahra falah <sepidehfallah2@gmail.com>
2018-07-08 10:10:40 -0700

    slyled

--- buddy/imports/ui/pages/welcomepage.js
+++ buddy/imports/ui/pages/welcomepage.js
@@ -6,7 +6,7 @@ import { Wave, Random } from "react-animated-text";
 const exampleStyle = {
     display: "inline-block",
     textAlign:"center",
-    backgroundColor:"yellow",   
+    backgroundColor:"#fafa84",   
     margin: "0 auto",
     paddingTop:"2.7em",    
     width:"100%",
@@ -14,12 +14,24 @@ const exampleStyle = {
     height:"100%",
     top:"0px",
     left:"0px",
-    zindex:"1000",
+    // zindex:"1000",
     fontSize: "8rem",
     // fontWeigh:700    
   };
 const skipStyle = {
   fontSize: "1rem",
+  position:"relative",
+  paddingTop:"450",
+  paddingLeft:"600"
+
+
+
+}
+
+const policyStyle = {
+  fontSize: "1rem",
+ 
+
 }
 
 export const Random1 = () => (
@@ -30,10 +42,17 @@ export const Random1 = () => (
         effectChange={2.0}
         effectDuration={0.3}
       />
-    
-    </div>,
 
-    <div style={skipStyle}>
-       <h1>Skip</h1>
+     <div style={skipStyle}>
+       Skip
     </div>
+
+    <div style={policyStyle}>
+    Privacy Policy
+    </div>
+    
+    </div>
+    
+
+   
   );
