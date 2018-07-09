1cba14238fe65e1df8519cd85394c5c98023cc2a
Briana Oursler <briana.oursler@gmail.com>
2018-07-09 15:04:41 -0700

    Add user mocking

--- buddy/imports/api/profiles/methods.tests.js
+++ buddy/imports/api/profiles/methods.tests.js
@@ -1,16 +1,59 @@
 import { Meteor } from 'meteor/meteor';
-// import SimpleSchema from 'meteor/aldeed:collection2'
-// import {Profiles} from './profiles.js';
-import { AssertionError } from 'assert';
-// import {addProfile} from './methods.js';
+import { AssertionError, doesNotReject } from 'assert';
+import'chai';
+import { Random } from 'meteor/random'
 import './methods.js';
 describe('Profile methods', () =>{
 	describe('Profile insert method', ()=> {
+		let userId = null;
+    let userFct = null;
+
+    const isDefined = function (target) {
+        chai.assert.isNotNull(target, "unexpected null value");
+        chai.assert.isDefined(target, "unexpected undefined value");
+
+        if (typeof target === 'string')
+            assert.notEqual(target.trim(), "");
+    };
+
+    //------------------------------------------//
+
+    beforeEach(() => {
+
+        // save the original user fct
+        userFct = Meteor.user;
+
+        // Generate a real user, otherwise it is hard to test roles
+        userId = Accounts.createUser({username: Random.id(5)});
+        isDefined(userId);
+
+        // mock the Meteor.user() function, so that it
+        // always returns our new created user
+        Meteor.user = function () {
+            const users = Meteor.users.find({_id: userId}).fetch();
+            if (!users || users.length > 1)
+                throw new Error("Meteor.user() mock cannot find user by userId.");
+            return users[0];
+        };
+    });
+
+    //------------------------------------------//
+
+    afterEach(() => {
+        //remove the user in the db
+        Meteor.users.remove(userId);
+        // restore user Meteor.user() function
+        Meteor.user = userFct;
+        // reset userId
+        userId = null;
+    });
+
 		it('inserts given data', () => {
 			try {
-			Meteor.call('profiles.addProfile',
-				'Briana', 'Johnson', 'A busy person')
+				Meteor.call('profiles.addProfile',
+					'Briana', 'Johnson', 'A busy person'), (err, res)=>{console.log(res)};
 				}
+			
 			catch(err){
 				throw new Error(err);
 			}
@@ -18,14 +61,3 @@ describe('Profile methods', () =>{
 		});
 	});
 
-
-
-//   describe('Profiles', () => {
-// 	describe('invalid insert', () => {
-// 		it('denies invalid inserts into a validated schema', () => {
-// 			console.log("Setup to run build");
-// 			ret = Profiles.simpleSchema().namedContext().validate({title: "Ulysses", author: "James Joyce"}, {modifier: false});
-// 			if(ret == true)
-// 				throw new Error("Failed invalid schema accepted as valid");
-// 		});
-// 	});
