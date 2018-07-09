976b4f0543c5538d87ff9b52866052fac6ae1141
Briana Oursler <briana.oursler@gmail.com>
2018-07-09 16:55:43 -0700

    Debugging profiles tests and profiles methods

--- buddy/imports/api/profiles/methods.tests.js
+++ buddy/imports/api/profiles/methods.tests.js
@@ -1,16 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import { AssertionError, doesNotReject } from 'assert';
-import'chai';
+import{assert} from 'chai';
 import { Random } from 'meteor/random'
 import './methods.js';
-describe('Profile methods', () =>{
+describe('Profile methods', function(){
 	describe('Profile insert method', ()=> {
 		let userId = null;
     let userFct = null;
 
     const isDefined = function (target) {
-        chai.assert.isNotNull(target, "unexpected null value");
-        chai.assert.isDefined(target, "unexpected undefined value");
+        assert.isNotNull(target, "unexpected null value");
+        assert.isDefined(target, "unexpected undefined value");
 
         if (typeof target === 'string')
             assert.notEqual(target.trim(), "");
@@ -18,7 +18,7 @@ describe('Profile methods', () =>{
 
     //------------------------------------------//
 
-    beforeEach(() => {
+    beforeEach(function(){
 
         // save the original user fct
         userFct = Meteor.user;
@@ -26,11 +26,12 @@ describe('Profile methods', () =>{
         // Generate a real user, otherwise it is hard to test roles
         userId = Accounts.createUser({username: Random.id(5)});
         isDefined(userId);
-
+		console.log(userId, "userId generated");
         // mock the Meteor.user() function, so that it
         // always returns our new created user
-        Meteor.user = function () {
-            const users = Meteor.users.find({_id: userId}).fetch();
+        Meteor.User = function () {
+			const users = Meteor.users.find({_id: userId}).fetch();
+			console.log("creating user");
             if (!users || users.length > 1)
                 throw new Error("Meteor.user() mock cannot find user by userId.");
             return users[0];
@@ -39,8 +40,9 @@ describe('Profile methods', () =>{
 
     //------------------------------------------//
 
-    afterEach(() => {
-        //remove the user in the db
+     afterEach(function(){
+		//remove the user in the db
+		console.log("removing");
         Meteor.users.remove(userId);
         // restore user Meteor.user() function
         Meteor.user = userFct;
@@ -48,8 +50,9 @@ describe('Profile methods', () =>{
         userId = null;
     });
 
-		it('inserts given data', () => {
+		it('inserts given data', function() {
 			try {
+				console.log("try block");
 				Meteor.call('profiles.addProfile',
 					'Briana', 'Johnson', 'A busy person'), (err, res)=>{console.log(res)};
 				}
