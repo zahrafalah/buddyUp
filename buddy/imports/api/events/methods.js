5dbab765669fe9bdcb6978cbb6ee6a3eedea0d3b
Aceituna <miriam_armada@yahoo.com>
2018-07-10 12:59:05 -0700

    update rebase

--- buddy/imports/api/events/methods.js
+++ buddy/imports/api/events/methods.js
@@ -45,3 +45,31 @@ Events.update({eventfulID: event.eventfulID}, {$set: event},{upsert: true}, (err
 	}
 });
 
+const POLL_INTERVAL = 5000;
+
+Meteor.publish('polled-publication', function() {
+  const publishedKeys = {};
+
+  const poll = () => {
+   
+    const data = HTTP.get(REST_URL, REST_OPTIONS);
+
+    data.forEach((doc) => {
+      if (publishedKeys[doc._id]) {
+        this.changed(COLLECTION_NAME, doc._id, doc);
+      } else {
+        publishedKeys[doc._id] = true;
+        this.added(COLLECTION_NAME, doc._id, doc);
+      }
+    });
+  };
+
+  poll();
+  this.ready();
+
+  const interval = Meteor.setInterval(poll, POLL_INTERVAL);
+
+  this.onStop(() => {
+    Meteor.clearInterval(interval);
+  });
+});
