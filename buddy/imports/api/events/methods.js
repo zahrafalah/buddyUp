b778e1e9ee41a16f735ed209744811109fec377c
Aceituna <miriam_armada@yahoo.com>
2018-07-10 21:36:16 -0700

    update method

---
+++ buddy/imports/api/events/methods.js
@@ -0,0 +1,12 @@
+import {Meteor} from 'meteor/meteor';
+
+Meteor.methods({
+    'geoJsonForIp': function (ip) {
+      // avoid blocking other method calls from the same client
+      this.unblock();
+      var apiUrl = 'http://api.eventful.com/json/events/search?...&app_key=Pn3g5RvNgRnxzTxp' + ip;
+      // asynchronous call to the dedicated API calling function
+      var response = Meteor.wrapAsync(apiCall)(apiUrl);
+      return response;
+    }
+  });
