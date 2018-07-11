62036195e8b7f275a5bca81386c82566eb2be440
Aceituna <miriam_armada@yahoo.com>
2018-07-10 17:20:19 -0700

    add components for Events page

---
+++ buddy/imports/ui/components/VerticalMenu.js
@@ -0,0 +1,87 @@
+import React, {Component} from "react";
+import AccountsUIWrapper from AccountsUIWrapper;
+
+
+class VerticalMenu extends Component {
+
+    render(){
+        return
+        <div class="ui vertical menu">
+            <div class="item">
+                <div class="ui input"><input type="text" placeholder="Anything..."/>Search Something
+            </div>
+            
+            <div class="item">
+                Categories
+                </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Music</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Outdoor Activities</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Arts</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Boardgames & Gambling</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Coffee</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Concerts</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Gaming</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Food</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Drinks and Happy Hours</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Holiday Theme Activities</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Health & Wellness</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Pets</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Books & Related</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Science & Related</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Sports</label>
+            </div>
+            <div class="ui checkbox">
+                <input type="checkbox" name="example"/>
+                <label>Volunteer</label>
+        </div>
+    </div>
+  </div>
+    };
+    
+};
+
+export default VerticalMenu
