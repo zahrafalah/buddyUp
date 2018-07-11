bc2c393aebfab1362d0e4d79123c76f91433a567
Aceituna <miriam_armada@yahoo.com>
2018-07-11 11:04:10 -0700

    update eventful api setup

--- buddy/imports/api/events/methods.js
+++ buddy/imports/api/events/methods.js
@@ -1,10 +1,11 @@
 import {Meteor} from 'meteor/meteor';
 
+
 Meteor.methods({
-    'geoJsonForIp': function (ip) {
+    'geoJsonForIp': function (query) {
       // avoid blocking other method calls from the same client
       this.unblock();
-      var apiUrl = 'http://api.eventful.com/json/events/search?...&app_key=Pn3g5RvNgRnxzTxp' + ip;
+      var apiUrl = 'http://api.eventful.com/json/events/search?...&app_key=Pn3g5RvNgRnxzTxp' + query;
       // asynchronous call to the dedicated API calling function
       var response = Meteor.wrapAsync(apiCall)(apiUrl);
       return response;
