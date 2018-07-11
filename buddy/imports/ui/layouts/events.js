52d8921d050d4c53e1159c8a2bef12504267d85c
Aceituna <miriam_armada@yahoo.com>
2018-07-10 17:55:06 -0700

    update events

--- buddy/imports/ui/layouts/events.js
+++ buddy/imports/ui/layouts/events.js
@@ -1,26 +1,3 @@
-// top level layout: will include things like navbar, button placement, areas of
-// text
-import PropTypes from "prop-types";
-import React, {Component} from "react";
-import Menu from "../components/Menu";
-import VerticalMenu from "../components/VerticalMenu";
-import Event from "../components/Event";
-import {addUrlProps, UrlQueryParamTypes} from 'react-url-query';
-import {
-    Button,
-    Divider,
-    Grid,
-    Container,
-    Header,
-    Icon,
-    Image,
-    List,
-    Responsive,
-    Segment,
-    Sidebar,
-    Visibility
-} from "semantic-ui-react";
-
 // top level layout: will include things like navbar, button placement, areas of text 
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
