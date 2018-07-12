1e2b1d223c805271a4617b799d28bba649d1ac61
Aceituna <miriam_armada@yahoo.com>
2018-07-12 15:18:20 -0700

    correction event.js

--- buddy/imports/ui/layouts/events.js
+++ buddy/imports/ui/layouts/events.js
@@ -21,7 +21,7 @@ import {
   Visibility
 } from "semantic-ui-react";
 
-class event extends React.Component {
+export default class event extends React.Component {
 
     state = {
         result: {},
@@ -120,4 +120,4 @@ class event extends React.Component {
         );
     };
 
-    export default event;
+};
