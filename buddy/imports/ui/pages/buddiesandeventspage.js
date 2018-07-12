ff7965a93980d7f25fd24d594e5aab180b414aad
Briana Oursler <briana.oursler@gmail.com>
2018-07-11 17:48:17 -0700

    Refactor code to call out to layouts/events

--- buddy/imports/ui/pages/buddiesandeventspage.js
+++ buddy/imports/ui/pages/buddiesandeventspage.js
@@ -1,8 +1,12 @@
 /*// top-level page, will call layout inside of here and I'm thinking logic will
 // happen here?
 import PropTypes from 'prop-types'
+<<<<<<< HEAD
 import React, {Component} from 'react'
 import events from '../layouts/EventsLayout'
+=======
+import React, { Component } from 'react'
+>>>>>>> 3e902ea... Refactor code to call out to layouts/events
 
 import {
     Button,
