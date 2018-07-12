c7692eaefaf5a0f57d81f29dba979159f7c94dbe
Aceituna <miriam_armada@yahoo.com>
2018-07-12 14:49:09 -0700

    update event.js

--- buddy/imports/ui/layouts/events.js
+++ buddy/imports/ui/layouts/events.js
@@ -1,14 +1,11 @@
-// top level layout: will include things like navbar, button placement, areas of text
+// top level layout: will include things like navbar, button placement, areas of
+// text
 import PropTypes from "prop-types";
 import React, {Component} from "react";
 import Menu from "../components/Menu";
 import VerticalMenu from "../components/VerticalMenu";
 import Event from "../components/Event";
-<<<<<<< Updated upstream
 import {addUrlProps, UrlQueryParamTypes} from 'react-url-query';
-=======
-
->>>>>>> Stashed changes
 import {
     Button,
     Divider,
@@ -26,133 +23,101 @@ import {
 
 class event extends React.Component {
 
-  state = {
-    result: {},
-    search: {}
-}
-  constructor () {
-    super();
-    this.state.search = {
-      search: '',
-      arts:'',
-      coffee: '',
-      food:'',
-      reading:'',
-      sports:'',
-      science:'',
-      concert:'',
-      pets:'',
-      festivals:'',
-      happyHour:'',
-      volunteer:'',
-      holiday:'',
-      zipCode:'',
-      distance:''|| 50,
-     
-    };
-  }
-
-  var query = '&q=' + this.search.search + 
-   
-
-  var query = '&q=' + this.search.search + 
-   
-
-    constructor(props) {
-
-        super(props);
+    state = {
+        result: {},
+        search: {}
+    }
+    constructor() {
+        super();
         this.state.search = {
             search: '',
-            animals: '',
-            art: '',
-            books: '',
-            festivals_parades: '',
+            arts: '',
+            coffee: '',
             food: '',
-            fundraisers: '',
-            holiday: '',
-            music: '',
-            outdoors_recreation: '',
-            science: '',
-            singles_social: '',
+            reading: '',
             sports: '',
-            location: '',
-            distance: ''
+            science: '',
+            concert: '',
+            pets: '',
+            festivals: '',
+            happyHour: '',
+            volunteer: '',
+            holiday: '',
+            zipCode: '',
+            distance: '' || 50
         };
 
         this.handleChange = this
             .handleChange
             .bind(this);
-
     };
-}
 
-handleChange(event) {
-    this
-        .search
-        .setState({
-            [event.target.name]: event.target.value
-        });
-};
+    handleChange = event => {
+        this
+            .search
+            .setState({
+                [event.target.name]: event.target.value
+            });
+    };
 
-searchEvents = query => {
+    searchEvents = query => {
 
-    Meteor
-        .call('geoJsonForIp', query, function (err, res) {
-            // The method call sets the Session variable to the callback value
-            if (err) {
-                Session.set(this.state.result, {error: err});
-            } else {
-                Session.set(this.state.result, res);
-                return res;
-            }
-        });
-}
-
-handleFormSubmit = event => {
-    const queryURL = {
-        search: {
-            type: UrlQueryParamTypes.string,
-            queryParam: this.state.search.search
-        },
-        'ex-category': {
-            type: UrlQueryParamTypes.string,
-            queryParam: 'attractions,comedy,community,family_fun_kids,movies_film,performing_arts,schools' +
-                    '_alumni,support,technology'
-        },
-        location: {
-            type: UrlQueryParamTypes.string,
-            queryParam: this.state.search.location
-        },
-        distance: {
-            type: UrlQueryParamTypes.string,
-            queryParam: this.state.search.distance
-        },
-        category: {
-            type: UrlQueryParamTypes.string,
-            queryParam:join(Object.value(this.state.search))
+        Meteor
+            .call('geoJsonForIp', query, function (err, res) {
+                // The method call sets the Session variable to the callback value
+                if (err) {
+                    Session.set(this.state.result, {error: err});
+                } else {
+                    Session.set(this.state.result, res);
+                    return res;
+                }
+            });
     }
-};
 
-    event.preventDefault();
-    this.searchEvents(queryURL);
-};
+    handleFormSubmit = event => {
+        const queryURL = {
+            search: {
+                type: UrlQueryParamTypes.string,
+                queryParam: this.state.search.search
+            },
+            'ex-category': {
+                type: UrlQueryParamTypes.string,
+                queryParam: 'attractions,comedy,community,family_fun_kids,movies_film,performing_arts,schools' +
+                        '_alumni,support,technology'
+            },
+            location: {
+                type: UrlQueryParamTypes.string,
+                queryParam: this.state.search.location
+            },
+            distance: {
+                type: UrlQueryParamTypes.string,
+                queryParam: this.state.search.distance
+            },
+            category: {
+                type: UrlQueryParamTypes.string,
+                queryParam: join(Object.value(this.state.search))
+            }
+        };
 
-render() {
-    return (
-        <Segment >
-            <Container>
-                <Menu/>
-            </Container>
-            < Container >
-                <div class="four wide column">
-                    <VerticalMenu/>
-                </div>
-                < div class="12 wide column">
-                    <Event/>{children}</div>
-            </Container >
-        </Segment>
-    );
-};
+        event.preventDefault();
+        this.searchEvents(queryURL);
+    };
 
+    render() {
+        return (
+            <Segment >
+                <Container>
+                    <Menu/>
+                </Container>
+                < Container >
+                    <div class="four wide column">
+                        <VerticalMenu/>
+                    </div>
+                    < div class="12 wide column">
+                        <Event/>{children}</div>
+                </Container >
+            </Segment>
+        );
+    };
 
-export default event;
+    export default event;
