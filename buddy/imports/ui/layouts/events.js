43636d41503292a85d2b236ecf487d129cf8e888
Aceituna <miriam_armada@yahoo.com>
2018-07-11 17:05:39 -0700

    API pre-set up with constructors for testing

--- buddy/imports/ui/layouts/events.js
+++ buddy/imports/ui/layouts/events.js
@@ -1,24 +1,25 @@
-// top level layout: will include things like navbar, button placement, areas of text
+// top level layout: will include things like navbar, button placement, areas of
+// text
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Menu from "../components/Menu";
-import VerticalMenu from "../components/VerticalMenu";
+import VerticalMenu from "../components/VerticalMgit enu";
 import Event from "../components/Event";
 
 import {
-  Button,
-  Divider,
-  Grid,
-  Container,
-  Header,
-  Icon,
-  Image,
-  List,
+    Button,
+    Divider,
+    Grid,
+    Container,
+    Header,
+    Icon,
+    Image,
+    List,
 
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility
+    Responsive,
+    Segment,
+    Sidebar,
+    Visibility
 } from "semantic-ui-react";
 
 class event extends React.Component {
@@ -55,72 +56,72 @@ class event extends React.Component {
             .bind(this);
 
     };
-
-    handleChange(event) {
-        this
-            .search
-            .setState({
-                [event.target.name]: event.target.value
-            });
-    };
-
-    searchEvents = query => {
-
-        Meteor
-            .call('geoJsonForIp', query, function (err, res) {
-                // The method call sets the Session variable to the callback value
-                if (err) {
-                    Session.set('search', {error: err});
-                } else {
-                    Session.set('search', res);
-                    return res;
-                }
-            });
-    }
-
-    handleFormSubmit = event => {
-        const queryURL = {
-            search: {
-                type: UrlQueryParamTypes.string,
-                queryParam: this.state.search.search
-            },
-            'ex-category': {
-                type: UrlQueryParamTypes.string,
-                queryParam: 'attractions,comedy,community,family_fun_kids,movies_film,performing_arts,schools' +
-                        '_alumni,support,technology'
-            },
-            location: {
-                type: UrlQueryParamTypes.string,
-                queryParam: this.state.search.location
-            },
-            distance: {
-                type: UrlQueryParamTypes.string,
-                queryParam: this.state.search.distance
-            }
-        };
-        event.preventDefault();
-
-        this.searchEvents(queryURL);
-    };
-
-    render() {
-        return (
-            <Segment >
-                <Container>
-                    <Menu/>
-                </Container>
-                < Container >
-                    <div class="four wide column">
-                        <VerticalMenu/>
-                    </div>
-                    < div class="12 wide column">
-                        <Event/>{children}</div>
-                </Container >
-            </Segment>
-        )
-    }
 }
 
+handleChange(event) {
+    this
+        .search
+        .setState({
+            [event.target.name]: event.target.value
+        });
+};
+
+searchEvents = query => {
+
+    Meteor
+        .call('geoJsonForIp', query, function (err, res) {
+            // The method call sets the Session variable to the callback value
+            if (err) {
+                Session.set('search', {error: err});
+            } else {
+                Session.set('search', res);
+                return res;
+            }
+        });
+}
+
+handleFormSubmit = event => {
+    const queryURL = {
+        search: {
+            type: UrlQueryParamTypes.string,
+            queryParam: this.state.search.search
+        },
+        'ex-category': {
+            type: UrlQueryParamTypes.string,
+            queryParam: 'attractions,comedy,community,family_fun_kids,movies_film,performing_arts,schools' +
+                    '_alumni,support,technology'
+        },
+        location: {
+            type: UrlQueryParamTypes.string,
+            queryParam: this.state.search.location
+        },
+        distance: {
+            type: UrlQueryParamTypes.string,
+            queryParam: this.state.search.distance
+        }
+    };
+    event.preventDefault();
+
+    this.searchEvents(queryURL);
+};
+
+render() {
+    return (
+        <Segment >
+            <Container>
+                <Menu/>
+            </Container>
+            < Container >
+                <div class="four wide column">
+                    <VerticalMenu/>
+                </div>
+                < div class="12 wide column">
+                    <Event/>{children}</div>
+            </Container >
+        </Segment>
+    );
+};
+
+};
+
 export default event;
-
-
--- buddy/imports/ui/pages/buddiesandeventspage.js
+++ buddy/imports/ui/pages/buddiesandeventspage.js
@@ -1,1 +1,30 @@
 //top-level page, will call layout inside of here and I'm thinking logic will happen here?
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+
+
+import {
+  Button,
+  Divider,
+  Grid,
+  Container,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
+import events from '../layouts/events';
+
+
+render() {
+return
+    
+<events/>
+
+};
