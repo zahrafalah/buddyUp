d20f9a8cdcb96ac6c27aeadf88e92424db7e169e
Aceituna <miriam_armada@yahoo.com>
2018-07-11 17:05:39 -0700

    API pre-set up with constructors for testing

--- buddy/imports/ui/pages/buddiesandeventspage.js
+++ buddy/imports/ui/pages/buddiesandeventspage.js
@@ -1,38 +1,33 @@
-/*// top-level page, will call layout inside of here and I'm thinking logic will
-// happen here?
+
+
+//top-level page, will call layout inside of here and I'm thinking logic will happen here?
 import PropTypes from 'prop-types'
-<<<<<<< HEAD
-import React, {Component} from 'react'
-import events from '../layouts/EventsLayout'
-=======
 import React, { Component } from 'react'
->>>>>>> 3e902ea... Refactor code to call out to layouts/events
+
+
 
 import {
-    Button,
-    Divider,
-    Grid,
-    Container,
-    Header,
-    Icon,
-    Image,
-    List,
-    Menu,
-    Responsive,
-    Segment,
-    Sidebar,
-    Visibility
+  Button,
+  Divider,
+  Grid,
+  Container,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
 } from 'semantic-ui-react'
-import EventsLayout from '../layouts/EventsLayout';
+import events from '../layouts/events';
 
 
-class buddiesandevents extends Component {
+render() {
+return
+    
+<events/>
 
-    render() {
-        return(
-          <EventsLayout/>
-        );
+};
 
-    };
-}
-export default buddiesandevents;*/
