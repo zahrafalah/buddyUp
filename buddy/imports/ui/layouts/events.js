bd896c130aa3d603805039724e14b4af922e7946
Aceituna <miriam_armada@yahoo.com>
2018-07-12 14:49:09 -0700

    update event.js

--- buddy/imports/ui/layouts/events.js
+++ buddy/imports/ui/layouts/events.js
@@ -1,4 +1,5 @@
-// top level layout: will include things like navbar, button placement, areas of text
+// top level layout: will include things like navbar, button placement, areas of
+// text
 import PropTypes from "prop-types";
 import React, {Component} from "react";
 import Menu from "../components/Menu";
@@ -22,127 +23,101 @@ import {
 
 class event extends React.Component {
 
-  state = {
-    result: {},
-    search: {}
-}
-  constructor () {
-    super();
-    this.state.search = {
-      search: '',
-      arts:'',
-      coffee: '',
-      food:'',
-      reading:'',
-      sports:'',
-      science:'',
-      concert:'',
-      pets:'',
-      festivals:'',
-      happyHour:'',
-      volunteer:'',
-      holiday:'',
-      zipCode:'',
-      distance:''|| 50,
-     
-    };
-  }
-
-  // var query = '&q=' + this.search.search + 
-// event: if isChecked === true
-//then this.setState({name: this.name})
-searchEvents = query => {
-  
-     Meteor.call('geoJsonForIp', query, function (err, res) {
-      // The method call sets the Session variable to the callback value
-      if (err) { 
-        Session.set('search', {error: err});
-      } else {
-        Session.set('search', res);
-        return res;
-      }
-    });
-  }
-
-  handleInputChange = event => {
-    const name = event.target.name;
-    const value = event.target.value;
-    this.setState({
-      q: event.target
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.searchEvents(this.state.search);
-  };
-
-
-
-  render() {
-    return;
-    <Segment>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <div class="four wide column">
-          <VerticalMenu />
-        </div>
-        <div class="12 wide column">
-         <Event/>
-        </div>
-      </Container>
-    </Segment>;
-  }
-}
-
-handleFormSubmit = event => {
-    const queryURL = {
-        search: {
-            type: UrlQueryParamTypes.string,
-            queryParam: this.state.search.search
-        },
-        'ex-category': {
-            type: UrlQueryParamTypes.string,
-            queryParam: 'attractions,comedy,community,family_fun_kids,movies_film,performing_arts,schools' +
-                    '_alumni,support,technology'
-        },
-        location: {
-            type: UrlQueryParamTypes.string,
-            queryParam: this.state.search.location
-        },
-        distance: {
-            type: UrlQueryParamTypes.string,
-            queryParam: this.state.search.distance
-        },
-        category: {
-            type: UrlQueryParamTypes.string,
-            queryParam:join(Object.value(this.state.search))
+    state = {
+        result: {},
+        search: {}
     }
-};
+    constructor() {
+        super();
+        this.state.search = {
+            search: '',
+            arts: '',
+            coffee: '',
+            food: '',
+            reading: '',
+            sports: '',
+            science: '',
+            concert: '',
+            pets: '',
+            festivals: '',
+            happyHour: '',
+            volunteer: '',
+            holiday: '',
+            zipCode: '',
+            distance: '' || 50
+        };
 
-    event.preventDefault();
-    this.searchEvents(queryURL);
-};
+        this.handleChange = this
+            .handleChange
+            .bind(this);
+    };
 
-render() {
-    return (
-        <Segment >
-            <Container>
-                <Menu/>
-            </Container>
-            < Container >
-                <div class="four wide column">
-                    <VerticalMenu/>
-                </div>
-                < div class="12 wide column">
-                    <Event/>{children}</div>
-            </Container >
-        </Segment>
-    );
-};
+    handleChange = event => {
+        this
+            .search
+            .setState({
+                [event.target.name]: event.target.value
+            });
+    };
 
-};
+    searchEvents = query => {
 
-export default event;
+        Meteor
+            .call('geoJsonForIp', query, function (err, res) {
+                // The method call sets the Session variable to the callback value
+                if (err) {
+                    Session.set(this.state.result, {error: err});
+                } else {
+                    Session.set(this.state.result, res);
+                    return res;
+                }
+            });
+    }
+
+    handleFormSubmit = event => {
+        const queryURL = {
+            search: {
+                type: UrlQueryParamTypes.string,
+                queryParam: this.state.search.search
+            },
+            'ex-category': {
+                type: UrlQueryParamTypes.string,
+                queryParam: 'attractions,comedy,community,family_fun_kids,movies_film,performing_arts,schools' +
+                        '_alumni,support,technology'
+            },
+            location: {
+                type: UrlQueryParamTypes.string,
+                queryParam: this.state.search.location
+            },
+            distance: {
+                type: UrlQueryParamTypes.string,
+                queryParam: this.state.search.distance
+            },
+            category: {
+                type: UrlQueryParamTypes.string,
+                queryParam: join(Object.value(this.state.search))
+            }
+        };
+
+        event.preventDefault();
+        this.searchEvents(queryURL);
+    };
+
+    render() {
+        return (
+            <Segment >
+                <Container>
+                    <Menu/>
+                </Container>
+                < Container >
+                    <div class="four wide column">
+                        <VerticalMenu/>
+                    </div>
+                    < div class="12 wide column">
+                        <Event/>{children}</div>
+                </Container >
+            </Segment>
+        );
+    };
+
+    export default event;
