9f141ec6817da711c004611cb60ba549e4aca983
Aceituna <miriam_armada@yahoo.com>
2018-07-12 19:50:39 -0700

    update to event

--- buddy/imports/ui/components/Event.js
+++ buddy/imports/ui/components/Event.js
@@ -1,19 +1,27 @@
 import React from React
 
-export default class Event  extends React.Component {
+const events = props.events.event;
 
- 
+export const Event = props => (
 
+    <div class="item" key={events.id}>
+        <div class="image">
+            <img src={events.image.medium.url}/>{props.children}
+        </div>
+        <div class="content">
+            <h3 class="header">{events.title}</h3>
+            <div class="meta">
+                <span class="date">{events.start_time}</span>
+                <span class="location">{events.city_name}, {events.region_name}</span>
+            </div>
 
-render(){
-/*we can create variables here for conditional rendering purposes or for styling purposes*/
-var example = event
-
-return(
-     <div>
-
-    {example == true ? <h1>Its true</h1> : <h1>its false</h1>}
+            <div class="description">
+                <p>{events.description}</p>
+            </div>
+            <div class="extra">
+                <a href={events.url} target="_blank">Additional Details</a>
+                <div class="ui label category">Pending</div>
+            </div>
+        </div>
     </div>
-    )
-  }
-}
+);
