c49946490856795c40fa7bc6a6b2fb2ee05bf7d3
Aceituna <miriam_armada@yahoo.com>
2018-07-12 22:15:39 -0700

    updae render

--- buddy/imports/ui/App.js
+++ buddy/imports/ui/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/HomePage';
 import { Random1 } from "./pages/welcomepage";
 import ProfilePage from './pages/ProfilePage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import EventPage from'.pages/EventPage';
+import EventsPage from'./pages/EventsPage';
 
 const styles = {
 
