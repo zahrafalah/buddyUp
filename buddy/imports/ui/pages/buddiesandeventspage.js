b3faa664c4e8291236cafb3064ae60e72819f604
Aceituna <miriam_armada@yahoo.com>
2018-07-12 20:44:07 -0700

    events layout

--- buddy/imports/ui/pages/buddiesandeventspage.js
+++ buddy/imports/ui/pages/buddiesandeventspage.js
@@ -1,46 +1,33 @@
-//top-level page, will call layout inside of here and I'm thinking logic will happen here?
+// top-level page, will call layout inside of here and I'm thinking logic will
+// happen here?
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import events from '../layouts/events.js'
 
-<<<<<<< Updated upstream
-=======
-
-
->>>>>>> Stashed changes
 import {
-  Button,
-  Divider,
-  Grid,
-  Container,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
+    Button,
+    Divider,
+    Grid,
+    Container,
+    Header,
+    Icon,
+    Image,
+    List,
+    Menu,
+    Responsive,
+    Segment,
+    Sidebar,
+    Visibility
 } from 'semantic-ui-react'
 import events from '../layouts/events';
 
-<<<<<<< Updated upstream
 class buddiesandevents extends Component {
-render() {
-return (
-    
-<events/>
-)
 
-};
+    render() {
+        return(
+          <events/>
+        )
+
+    };
 }
 export default buddiesandevents;
-=======
-
-render() {
-return
-    
-<events/>
-
-};
->>>>>>> Stashed changes
