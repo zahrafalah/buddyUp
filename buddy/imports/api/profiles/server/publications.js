0757cf840d28342fa8e2ce24ffb6de8796595de9
Briana Oursler <briana.oursler@gmail.com>
2018-07-17 15:07:26 -0700

    Draft publication to expose buddy/event relationship

--- buddy/imports/api/profiles/server/publications.js
+++ buddy/imports/api/profiles/server/publications.js
@@ -2,8 +2,13 @@ import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 import {Profiles} from '../profiles.js';
 
-Meteor.publish('tasks', function profilesPublication() {
+Meteor.publish('profiles', function profilesPublication() {
     return Profiles.find({
             userId: this.userId ,
         });
   });
+//goal is to use cultofcoders:grapher to get linked documents and expose them
+  Meteor.publish('buddies', function eventBuddiesPublication(){
+      const buddyLink = Profiles.getLink(profileId, 'profiles'); 
+      buddyLink.find().fetch();
+  })
