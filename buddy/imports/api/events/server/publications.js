a8b2e3064a1edf859e754f58613cf013bc43edee
Briana Oursler <briana.oursler@gmail.com>
2018-07-18 14:35:59 -0700

    Add check for signed-in user

--- buddy/imports/api/events/server/publications.js
+++ buddy/imports/api/events/server/publications.js
@@ -4,7 +4,13 @@ import {Events} from '../profiles.js';
 
 Meteor.publish('events', function eventsPublication() {
 	const eventsLink = Events.getLink(profileIds, 'profiles');
+	if (!this.userId) {
+		throw new Meteor.Error('not-authorized');
+}
 	return eventsLink.find().fetch();
   });
 //goal is to use cultofcoders:grapher to get linked documents and expose them
  
+// return Rooms.find({ admin: this.userId }, {
+// 	fields: { secretInfo: 1 }
+// });
