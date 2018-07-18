3c0e761365b780835ff792f642066d957042e628
Briana Oursler <briana.oursler@gmail.com>
2018-07-17 22:11:57 -0700

    Remove unnecessary import of collection2

--- buddy/imports/api/events/events.js
+++ buddy/imports/api/events/events.js
@@ -1,6 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
-import {Collection2} from 'meteor/aldeed:collection2'
 import SimpleSchema from 'simpl-schema';
 // import SimpleSchema from 'meteor/aldeed:collection2'
 export const Events = new Mongo.Collection('events');
